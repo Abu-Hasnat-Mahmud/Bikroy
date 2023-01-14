eff8f3fd5f43281b4565f07c1602d865870effa2
abuha <abuha@DESKTOP-DOSBFKK>
2023-01-14 13:21:04 +0600

    Add project files.

---
+++ Persistence/DAL/AppConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.DAL
+{
+    public class AppConfig
+    {
+        // Local Database
+        public static string DefaultConnection = "Server=DESKTOP-DOSBFKK;Database=Bikroy_DB;User Id=sa;Password=Ult72349$;Integrated Security=true;Trusted_Connection=False;MultipleActiveResultSets=true;";
+
+    }
+}
